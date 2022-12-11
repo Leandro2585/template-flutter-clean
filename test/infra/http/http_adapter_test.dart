19582c342b275fe481b29051d1fbf166f980adae
leandro2585 <leo.real2585@gmail.com>
2022-12-11 10:40:12 -0300

    feat: move HttpAdapter to its own file

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,35 +1,9 @@
-import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:http/http.dart';
-import 'package:meta/meta.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 
-import 'package:flutter_clean/data/http/http.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({
-    @required String url,
-    @required String method,
-    Map body
-  }) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(Uri.parse(url), headers: headers, body: jsonBody);
-    if(response.statusCode == 200) {
-      return response.body.isNotEmpty ? jsonDecode(response.body) : null;
-    } else {
-      return null;
-    }
-  }
-}
+import 'package:flutter_clean/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
 
