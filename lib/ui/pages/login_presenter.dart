4c25f6f99fb93e599c05a6da554d4b7e613eb65c
leandro2585 <leo.real2585@gmail.com>
2022-12-24 08:36:54 -0300

    refactor: make LoginPresenter auth async

--- lib/ui/pages/login_presenter.dart
+++ lib/ui/pages/login_presenter.dart
@@ -1,12 +1,12 @@
 abstract class LoginPresenter {
-  Stream get emailErrorStream;
-  Stream get passwordErrorStream;
-  Stream get mainErrorStream;
-  Stream get isFormValidStream;
-  Stream get isLoadingStream;
+  Stream<String> get emailErrorStream;
+  Stream<String> get passwordErrorStream;
+  Stream<String> get mainErrorStream;
+  Stream<bool> get isFormValidStream;
+  Stream<bool> get isLoadingStream;
 
+  Future<void> auth();
   void validateEmail(String email);
   void validatePassword(String password);
-  void auth();
   void dispose();
 }
