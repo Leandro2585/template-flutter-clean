096cbf9ac728618559486c8f3a810b1259ed1351
leandro2585 <leo.real2585@gmail.com>
2023-01-01 21:23:53 -0300

    refactor: move login validators creation to use ValidationBuilder

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_clean/application/protocols/protocols.dart';
 import 'package:flutter_clean/application/validators/validators.dart';
+import 'package:flutter_clean/main/builders/validation_builder.dart';
 
 Validation makeLoginValidation() {
   return ValidationComposite(makeLoginValidations());
@@ -7,8 +8,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build()
   ];
 }
