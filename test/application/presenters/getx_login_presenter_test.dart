57d264be81cba710ecca4962bf5762daee29c229
leandro2585 <leo.real2585@gmail.com>
2023-01-12 17:12:17 -0300

    test: ensure GetXLoginPresenter emits error if SaveCurrentAccount fails

--- test/application/presenters/getx_login_presenter_test.dart
+++ test/application/presenters/getx_login_presenter_test.dart
@@ -40,6 +40,13 @@ void main() {
     mockAuthenticationCall().thenThrow(error);
   }
 
+  PostExpectation mockSaveCurrentAccountCall() =>
+      when(saveCurrentAccount.save(any));
+
+  void makeSaveCurrentAccountError() {
+    mockAuthenticationCall().thenThrow(DomainError.unexpected);
+  }
+
   setUp(() {
     email = faker.internet.email();
     password = faker.internet.password();
@@ -158,6 +165,19 @@ void main() {
     verify(saveCurrentAccount.save(AccountEntity(token))).called(1);
   });
 
+  test('should emit UnexpectedError if SaveCurrentAccount fails', () async {
+    makeSaveCurrentAccountError();
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    sut.mainErrorStream.listen(expectAsync1((error) =>
+        expect(error, 'Algo errado aconteceu, tente novamente em breve.')));
+
+    await sut.auth();
+  });
+
   test('should emit correct events on Authentication success', () async {
     sut.validateEmail(email);
     sut.validatePassword(password);
@@ -178,7 +198,6 @@ void main() {
 
     await sut.auth();
   });
-
   test('should emit correct events on UnexpectedError', () async {
     mockAuthenticationError(DomainError.unexpected);
     sut.validateEmail(email);
