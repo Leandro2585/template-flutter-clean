91b89dc19e27abf925543226ee30b7290b56c4b9
leandro2585 <leo.real2585@gmail.com>
2023-01-25 10:44:02 -0300

    feat: move SplashPage to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean/ui/pages/splash/splash.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({Key key, @required this.presenter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Clean')),
+      body: Builder(builder: (context) {
+        presenter.navigateToStream.listen((page) {
+          if (page?.isNotEmpty == true) {
+            Get.offAllNamed(page);
+          }
+        });
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }),
+    );
+  }
+}
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,38 +1,11 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-class SplashPage extends StatelessWidget {
-  final SplashPresenter presenter;
-
-  const SplashPage({Key key, @required this.presenter}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    presenter.loadCurrentAccount();
-    return Scaffold(
-      appBar: AppBar(title: const Text('Flutter Clean')),
-      body: Builder(builder: (context) {
-        presenter.navigateToStream.listen((page) {
-          if (page?.isNotEmpty == true) {
-            Get.offAllNamed(page);
-          }
-        });
-        return const Center(
-          child: CircularProgressIndicator(),
-        );
-      }),
-    );
-  }
-}
-
-abstract class SplashPresenter {
-  Stream<String> get navigateToStream;
-  Future<void> loadCurrentAccount();
-}
+import 'package:flutter_clean/ui/pages/pages.dart';
 
 class SplashPresenterSpy extends Mock implements SplashPresenter {}
 
