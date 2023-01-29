57a9aacbca8bd52843cc9717aa78d9e2d4876e5b
leandro2585 <leo.real2585@gmail.com>
2023-01-29 20:31:46 -0300

    test: ensure GetxSplashPresenter emits login page on empty result

--- test/application/presenters/getx_splash_presenter_test.dart
+++ test/application/presenters/getx_splash_presenter_test.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
+import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:flutter_clean/ui/pages/pages.dart';
 import 'package:flutter_clean/domain/usecases/usecases.dart';
+import 'package:flutter_clean/domain/entities/entities.dart';
 
 class GetxSplashPresenter implements SplashPresenter {
   final LoadCurrentAccount loadCurrentAccount;
@@ -17,8 +19,8 @@ class GetxSplashPresenter implements SplashPresenter {
 
   @override
   Future<void> checkAccount() async {
-    await loadCurrentAccount.load();
-    _navigateTo.value = '/surveys';
+    final account = await loadCurrentAccount.load();
+    _navigateTo.value = account == null ? '/login' : '/surveys';
   }
 }
 
@@ -28,9 +30,14 @@ void main() {
   LoadCurrentAccountSpy loadCurrentAccount;
   GetxSplashPresenter sut;
 
+  void mockLoadCurrentAccount({AccountEntity account}) {
+    when(loadCurrentAccount.load()).thenAnswer((_) => null);
+  }
+
   setUp(() {
     loadCurrentAccount = LoadCurrentAccountSpy();
     sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+    mockLoadCurrentAccount(account: AccountEntity(token: faker.guid.guid()));
   });
 
   test('should call LoadCurrentAccount', () async {
@@ -42,6 +49,14 @@ void main() {
   test('should go to surveys page on success', () async {
     sut.navigateToStream
         .listen(expectAsync1((page) => expect(page, '/surveys')));
+
+    await sut.checkAccount();
+  });
+
+  test('should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: null);
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
 
     await sut.checkAccount();
   });
